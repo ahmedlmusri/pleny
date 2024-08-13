7434a70fce54e9d06092abc302429ec07e82dc9d
ahmed ehab <>
2024-08-13 18:22:39 +0300

    create product list task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren:()=> import('./components/authentication/authentication.module').then(m=> m.AuthenticationModule)
+  }
+  ,
+  {
+    path:'product-list',
+    loadChildren:()=> import('./components/products/products.module').then(m=> m.ProductsModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path:'**',
+    loadChildren:()=> import('./components/authentication/authentication.module').then(m=> m.AuthenticationModule)
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AppStoreModule } from './store/store.module';
+import { HttpClientModule } from '@angular/common/http';  // Import HttpClientModule
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AppStoreModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
